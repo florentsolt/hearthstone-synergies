0256fbe22e4e7a8d30b0d568e5bd2963e74e7101
Florent Solt <florent@solt.biz>
2014-04-24 20:38:43 +0200

    First shot on warrior & click on a card

--- js/synergies.js
+++ js/synergies.js
@@ -1,10 +1,11 @@
 var synergies = {
   heal:             1,
-  damage_friend:  2,
+  damage_friend:    2,
   kill:             3,
   armor:            4,
   weapon:           5,
   damage_enemy:     6,
+  summon:           7,
 };
 
 for (var id in synergies) {
@@ -17,12 +18,14 @@ var cards_synergies = {
   * Warrior
   ****************************************************/
 
+  // Armor Up!
+  725:  {trigger: [armor]},
   // Inner Rage
   22:   {trigger: [damage_friend, damage_enemy]},
   // Execute
   785:  {trigger: [kill], listen: [damage_enemy]},
   // Shied Slam
-  1074: {trigger: [damage_friend, damage_enemy], listen: [armor]},
+  546: {trigger: [damage_friend, damage_enemy], listen: [armor]},
   // Upgrade!!
   511:  {trigger: [weapon, damage_enemy], listen: [weapon]},
   // Whirlwind
@@ -31,15 +34,42 @@ var cards_synergies = {
   401:  {listen: [weapon], trigger: [damage_enemy]},
   // Battle Rage
   400:  {listen: [damage_friend]},
-  // Armorsmith
-  596:  {listen: [damage_friend]},
   // Cleave
   940:  {trigger: [damage_enemy]},
   // Commanding Shout
   1026: {listen: [damage_friend]},
+  // Heroic Strike
+  1007: {trigger: [damage_enemy]},
+  // Rampage
+  1108: {listen: [damage_friend]},
+  // Slam
+  1074: {trigger: [damage_friend, damage_enemy]},
+  // Armorsmith
+  596:  {trigger: [armor], listen: [damage_friend]},
   // Cruel Taskmaster
   285:  {trigger: [damage_friend, damage_enemy]},
-
+  // Charge
+  344:  {},
+  // Shield Block
+  1023: {trigger: [armor]},
+  // Frothing Berserker
+  654:  {listen: [damage_friend, damage_enemy]},
+  // Warsong Commander
+  1009: {listen: [summon]}, /* FIXME */
+  // Mortal Strike
+  804:  {trigger: [damage_enemy]},
+  // Arathi Weaponsmith
+  538:  {trigger: [weapon, damage_enemy]},
+  // Kor'kron Elite
+  28:   {},
+  // Arcanite Reaper
+  304:  {trigger: [weapon, damage_enemy]},
+  // Brawl
+  75:   {},
+  // Gorehowl
+  810:  {trigger: [weapon, damage_enemy]},
+  // Grommash Hellscream
+  338:  {listen: [damage_friend]}
 
 }
 
